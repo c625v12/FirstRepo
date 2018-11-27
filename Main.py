6e40104c8ce0e09daec834273e5a83ec2c2bf627
christophervalko <c625v12@github.com>
2018-11-27 17:33:22 -0500

    Changed how to select mode of math

--- Main.py
+++ Main.py
@@ -3,11 +3,11 @@ print("Matt Becker is a bitch")
 
 def adding_subtracting(first_num, second_num):
 
-    choice = input("Do you want to add or subtract?")
-    if choice == "subtract":
+    choice = input("Do you want to add or subtract? 1 for add or 2 for subtract")
+    if choice == "2":
 
         return print(first_num - second_num)
-    elif choice == "add":
+    elif choice == "1":
 
         return print(int(first_num) + int(second_num))
 
@@ -15,6 +15,6 @@ def adding_subtracting(first_num, second_num):
 if __name__ == "__main__":
 
     print("Enter two numbers")
-    firstnum = input("First number")
-    secondnum = input("Second number")
-    adding_subtracting(firstnum, secondnum)
+    first_num = input("First number")
+    second_num = input("Second number")
+    adding_subtracting(first_num, second_num)
