3ce8b910a90264f0b795c04fe4956af8bc49e065
christophervalko <c625v12@github.com>
2018-11-27 17:29:48 -0500

    Added function for adding and subtracting

--- Main.py
+++ Main.py
@@ -1,1 +1,20 @@
 print("Matt Becker is a bitch")
+
+
+def adding_subtracting(first_num, second_num):
+
+    choice = input("Do you want to add or subtract?")
+    if choice == "subtract":
+
+        return print(first_num - second_num)
+    elif choice == "add":
+
+        return print(int(first_num) + int(second_num))
+
+
+if __name__ == "__main__":
+
+    print("Enter two numbers")
+    firstnum = input("First number")
+    secondnum = input("Second number")
+    adding_subtracting(firstnum, secondnum)
