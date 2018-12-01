95a84c84f5ccee60dac10fccc291f8000a27645d
c625v12 <cjvalko@comcast.net>
2018-12-01 12:10:11 -0500

    Added method for add and subtract

--- Main.py
+++ Main.py
@@ -1,15 +1,14 @@
 print("Matt Becker is a bitch")
 
 
-def adding_subtracting(first_num, second_num):
+def adding(first_num, second_num):
 
-    choice = input("Do you want to add or subtract? 1 for add or 2 for subtract")
-    if choice == "2":
+    return print(first_num + second_num)
 
-        return print(first_num - second_num)
-    elif choice == "1":
 
-        return print(int(first_num) + int(second_num))
+def subtract(first_num, second_num):
+
+        return print(int(first_num) - int(second_num))
 
 
 if __name__ == "__main__":
@@ -17,4 +16,11 @@ if __name__ == "__main__":
     print("Enter two numbers")
     first_num = input("First number")
     second_num = input("Second number")
-    adding_subtracting(first_num, second_num)
+    choice = input("Do you want to add or subtract? 1 for add or 2 for subtract")
+    if choice == "1":
+
+        print(adding(int(first_num) - int(second_num)))
+
+    elif choice == "2":
+
+        print(subtract(int(first_num), int(second_num)))
