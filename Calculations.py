8b5283dd38c1ba2b6bce02e961b59cc4d0d82022
c625v12 <cjvalko@comcast.net>
2018-12-01 12:22:49 -0500

    Adding class for calculations and fixed syntax

---
+++ Calculations.py
@@ -0,0 +1,8 @@
+def adding(first_num_add, second_num_add):
+
+    return print(first_num_add + second_num_add)
+
+
+def subtract(first_num_sub, second_num_sub):
+
+        return print(int(first_num_sub) - int(second_num_sub))
--- Main.py
+++ Main.py
@@ -1,17 +1,11 @@
+import Calculations
+
 print("Matt Becker is a bitch")
 
 
-def adding(first_num_add, second_num_add):
-
-    return print(first_num_add + second_num_add)
-
-
-def subtract(first_num_sub, second_num_sub):
-
-        return print(int(first_num_sub) - int(second_num_sub))
-
-
 if __name__ == "__main__":
+
+    calc = Calculations
 
     print("Enter two numbers")
     first_num = input("First number")
@@ -19,8 +13,8 @@ if __name__ == "__main__":
     choice = input("Do you want to add or subtract? 1 for add or 2 for subtract")
     if choice == "1":
 
-        print(adding(int(first_num) - int(second_num)))
+        print(calc.adding(int(first_num), int(second_num)))
 
     elif choice == "2":
 
-        print(subtract(int(first_num), int(second_num)))
+        print(calc.subtract(int(first_num), int(second_num)))
